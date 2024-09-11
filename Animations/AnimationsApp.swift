d345ccc2e897f93934e1b4a23a323f24e04cf268
Devy <izabela.marcinkowska@outlook.com>
2024-09-11 13:09:13 +0200

    First commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Izabela Marcinkowska on 2024-09-11.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
