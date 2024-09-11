d3cf495a2b1a12e31920183cec956d78690c5c13
Devy <izabela.marcinkowska@outlook.com>
2024-09-11 13:18:49 +0200

    Simple implicit animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Tap me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.teal)
+            .foregroundColor(.black)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount)
+            .blur(radius: (animationAmount - 1) * 3)
+            .animation(.default, value: animationAmount)
         }
         .padding()
     }
