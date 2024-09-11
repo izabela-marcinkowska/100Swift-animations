e763309536591605adc954186bd9de29cfafc76d
Devy <izabela.marcinkowska@outlook.com>
2024-09-11 13:26:42 +0200

    feat: Animation with bounce

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -21,6 +21,16 @@ struct ContentView: View {
             .scaleEffect(animationAmount)
             .blur(radius: (animationAmount - 1) * 3)
             .animation(.default, value: animationAmount)
+            Button("Tap me 2") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.teal)
+            .foregroundColor(.black)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount)
+            .blur(radius: (animationAmount - 1) * 3)
+            .animation(.spring(duration: 1, bounce: 0.9), value: animationAmount)
         }
         .padding()
     }
