27b160cc5a6736613b9bf465a0600c56feefb1b2
Devy <izabela.marcinkowska@outlook.com>
2024-09-11 13:55:37 +0200

    feat: Pulsating circle and overlay

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,56 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 0.3
+    @State private var animationAmount = 0.5
     var body: some View {
         VStack {
             Button("Tap me") {
-                animationAmount += 1
+//                animationAmount += 1
             }
             .padding(50)
             .background(.teal)
             .foregroundColor(.black)
             .clipShape(.circle)
-            .scaleEffect(animationAmount)
-            .blur(radius: (animationAmount - 1) * 3)
-            .animation(.default, value: animationAmount)
-            
-            Button("Tap me 2") {
-                animationAmount += 1
+            .overlay(
+                Circle()
+                    .stroke(.red)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+                    .animation( .easeOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount)
+            )
+            .onAppear{
+                animationAmount = 2
             }
-            .padding(50)
-            .background(.teal)
-            .foregroundColor(.black)
-            .clipShape(.circle)
-            .scaleEffect(animationAmount)
-            .blur(radius: (animationAmount - 1) * 3)
-            .animation(.spring(duration: 1, bounce: 0.9), value: animationAmount)
-            
-            Button("Tap me 3") {
-                animationAmount += 1
-            }
-            .padding(50)
-            .background(.teal)
-            .foregroundColor(.black)
-            .clipShape(.circle)
-            .scaleEffect(animationAmount)
-            .blur(radius: (animationAmount - 1) * 3)
-            .animation(.easeInOut(duration: 2)
-                .delay(1),
-                       value: animationAmount)
-            
-            Button("Tap me 4") {
-                animationAmount += 1
-            }
-            .padding(50)
-            .background(.teal)
-            .foregroundColor(.black)
-            .clipShape(.circle)
-            .scaleEffect(animationAmount)
-            .blur(radius: (animationAmount - 1) * 3)
-            .animation(.easeInOut(duration: 1)
-                .repeatCount(3, autoreverses: true),
-                       value: animationAmount)
+           
         }
         .padding()
     }
