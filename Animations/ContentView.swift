0b0d80052c4ef9968d3a1af24627bade6a45e243
Devy <izabela.marcinkowska@outlook.com>
2024-09-12 10:59:39 +0200

    Explicit animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -11,28 +11,19 @@ struct ContentView: View {
     @State private var animationAmount = 0.5
     var body: some View {
         VStack {
-            Button("Tap me") {
-//                animationAmount += 1
+            Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
             }
-            .padding(50)
-            .background(.teal)
-            .foregroundColor(.black)
+            .padding(40)
+            .background(.red)
+            .foregroundStyle(.white)
             .clipShape(.circle)
-            .overlay(
-                Circle()
-                    .stroke(.red)
-                    .scaleEffect(animationAmount)
-                    .opacity(2 - animationAmount)
-                    .animation( .easeOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                    value: animationAmount)
-            )
-            .onAppear{
-                animationAmount = 2
-            }
-           
+            .scaleEffect(animationAmount)
         }
-        .padding()
     }
 }
 
